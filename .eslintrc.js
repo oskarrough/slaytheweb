57307d410e4445cd288e29bf37ef3c62ceb9392f
Oskar Rough <oskar@rough.dk>
2019-04-06 16:47:15 +0200

    Initial something

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	parserOptions: {
+		sourceType: 'module'
+	},
+	plugins: ["prettier"],
+	// rules: {
+	// 	"prettier/prettier": "error"
+	// }
+	// extends: ["plugin:prettier/recommended"]
+}
