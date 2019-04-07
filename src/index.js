45c3c6de82ed1f3e36bead8118732f14a52a1cc0
Oskar Rough <oskar@rough.dk>
2019-04-07 22:43:39 +0200

    Alrighty

--- src/index.js
+++ src/index.js
@@ -2,19 +2,31 @@
 // import {html, render, Component} from "https://unpkg.com/htm/preact/standalone.mjs"
 import {html, render, Component} from './htm-preact-standalone.mjs'
 import models from './models.js'
-import {Player, Cards} from './components/all.js'
+import Player from './components/player.js'
+import Cards from './components/cards.js'
 
 class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			player: {
+				maxEnergy: 3,
+				currentEnergy: 3,
+				maxHealth: 100,
+				currentHealth: 10
+			},
+			cards: models.cards
+		}
+	}
 	render(props, state) {
 		return html`
 			<div class="App">
 				<div class="u-flex">
-					<${Player} name="1">
-					</Player>
+					<${Player} name="1" />
 					<${Player} name="2" />
 				</div>
-				<${Cards} cards=${models.cards} />
-				<div class="DiscardPile">Disard here</div>
+				<div class="DiscardPile Cards"></div>
+				<${Cards} cards=${state.cards} />
 			</div>
 		`
 	}
@@ -29,26 +41,35 @@ render(
 	rootEl
 )
 
-const playerDeck = document.querySelector('.Cards')
-const discardPile = document.querySelector('.DiscardPile')
+setTimeout(gogo, 2000)
 
-dragula([playerDeck, discardPile], {
-	copy: function (el, source) {
-	  return source === playerDeck
-	},
-	accepts: function (el, target) {
-	  return target !== playerDeck
-	}
-})
-	.on('drag', function(el) {
-		el.className = el.className.replace('ex-moved', '')
+function gogo() {
+	const playerDeck = document.querySelector('.PlayerDeck')
+	const discardPile = document.querySelector('.DiscardPile')
+
+	dragula([playerDeck, discardPile], {
+		moves(el, source, handle, sibling) {
+			// elements are always draggable by default
+			return true
+		},
+		copy(el, source) {
+			return source === playerDeck
+		},
+		accepts(el, target) {
+			return target !== playerDeck
+		}
 	})
-	.on('drop', function(el) {
-		el.className += ' ex-moved'
-	})
-	.on('over', function(el, container) {
-		container.className += ' ex-over'
-	})
-	.on('out', function(el, container) {
-		container.className = container.className.replace('ex-over', '')
-	})
+		.on('drag', function(el) {
+			el.className = el.className.replace('ex-moved', '')
+		})
+		.on('drop', function(el, target, source) {
+			console.log(el, target)
+			el.className += ' ex-moved'
+		})
+		.on('over', function(el, container) {
+			container.className += ' ex-over'
+		})
+		.on('out', function(el, container) {
+			container.className = container.className.replace('ex-over', '')
+		})
+}
