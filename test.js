f330b9f390ea14c667618ab2f9a1c9c8615d901a
Oskar Rough <oskar@rough.dk>
2020-02-03 08:27:27 +0100

    Add our first real test

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const test = require('ava')
+import actions from './public/game/actions'
 
 test('foo', t => {
 	t.pass()
@@ -7,4 +8,32 @@ test('foo', t => {
 test('bar', async t => {
 	const bar = Promise.resolve('bar')
 	t.is(await bar, 'bar')
+})
+
+test('new game state is ok', t => {
+	const state = actions.createNewGame()
+	t.deepEqual(state, {
+		deck: [],
+		hand: [],
+		discardPile: [],
+		player1: {
+			maxEnergy: 3,
+			currentEnergy: 3,
+			maxHealth: 100,
+			currentHealth: 100
+		},
+		player2: {
+			maxEnergy: 3,
+			currentEnergy: 3,
+			maxHealth: 42,
+			currentHealth: 42
+		}
+	})
+})
+
+test('drawing a starter deck actually adds it to the deck', t => {
+	const state = actions.createNewGame()
+	t.is(state.deck.length, 0)
+	const newState = actions.drawStarterDeck({state})
+	t.true(newState.deck.length > 3)
 })
