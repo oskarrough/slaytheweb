588cf793c7c3cf553ce73c05b468fa38ee89c806
Oskar Rough <oskar@rough.dk>
2020-02-07 21:25:24 +0100

    Rotate the cards for that feel

--- public/components/cards.js
+++ public/components/cards.js
@@ -1,6 +1,11 @@
 import {html, Component} from './../web_modules/htm/preact/standalone.module.js'
 
 export default class Cards extends Component {
+	componentDidMount() {
+		const cards = this.base.querySelectorAll('.Card')
+		const handWidth = this.base.offsetWidth
+		positionCards(cards, handWidth)
+	}
 	render({cards, canDrag, isDiscardPile}) {
 		const classNames = `Cards ${canDrag ? 'dropzone' : ''} ${isDiscardPile ? 'Cards--discard' : ''}`
 		return html`
@@ -13,28 +18,24 @@ export default class Cards extends Component {
 
 export const Card = ({id, name, type, energy, effects}) => html`
 	<article class="Card" data-id=${id}>
-		<h3 class="Card-title">${name}</h3>
-		<p class="Card-energy">${energy}</p>
-		<!-- <p class="Card-type">${type}</p> -->
-		<p class="Card-effects">${effects}</p>
+		<div class="Card-inner">
+			<h3 class="Card-title">${name}</h3>
+			<p class="Card-energy">${energy}</p>
+			<!-- <p class="Card-type">${type}</p> -->
+			<p class="Card-effects">${effects}</p>
+		</div>
 	</article>
 `
 
-// function cardTransform(offset, handWidth) {
-// 	const transform =
-// 		'rotate(' + offset * 4 + 'deg) translateX(' +
-// 		(offset - (Math.abs(offset) * offset) / 7) * Math.min(140, handWidth / 8) +
-// 		'px)'
-// 	return transform
-// }
+function cardTransform(offset, handWidth) {
+	return `rotate(${offset * 4}deg)`
+}
 
-// function positionCards() {
-// 	const cards = this.base.querySelectorAll('.Card')
-// 	// if (cards.length) return
-// 	const handWidth = this.base.offsetWidth
-// 	cards.forEach((card, index) => {
-// 		const offset = parseInt(index, 10) - 2
-// 		console.log(offset, handWidth)
-// 		card.style.transform = cardTransform(offset, handWidth)
-// 	})
-// }
+function positionCards(cards, width) {
+	if (!cards.length) return
+	cards.forEach((card, index) => {
+		const offset = parseInt(index, 10) - 2
+		console.log(offset, width)
+		card.style.transform = cardTransform(offset, width)
+	})
+}
