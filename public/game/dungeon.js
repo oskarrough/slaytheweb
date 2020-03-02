a02777ec1ef37d099a98430f7c490661260b89bc
Oskar Rough <oskar@rough.dk>
2020-03-02 13:25:58 +0100

    Implement new classes: Dungeon, (Camp,Monster)Room, Monster

--- public/game/dungeon.js
+++ public/game/dungeon.js
@@ -1,56 +1,75 @@
-// A dungeon consists of different encounters and campfires.
-// An encounter is a fight against one or more monsters.
+import {uuid} from './utils.js'
+
+// A dungeon is where the adventure starts. It has a path of rooms, you know where you are and you can continue to the next room.
+export default class Dungeon {
+	constructor(props) {
+		if (props.rooms.length) this.rooms = props.rooms
+		this.currentIndex = 0
+	}
+	get currentRoom() {
+		return this.rooms[this.currentIndex]
+	}
+	goToNextRoom() {
+		if (this.currentIndex === this.rooms.length - 1) throw new Error('Already at last room')
+		this.currentIndex++
+	}
+}
+
+// Room types: Monster, Elite, Boss, Campfire, Shop, Treasure and Event
+class Room {
+	constructor(type) {
+		this.type = type
+		this.id = uuid()
+	}
+	get isComplete() {
+		return false
+	}
+}
+
 // A campfire gives our hero the opportunity to rest, remove or upgrade a card.
-
-const exampleDungeon = [
-	encounter1,
-	campfire,
-	encounter2,
-	encounter3,
-	campfire,
-	encounter2
-]
-
-// Either rest, upgrade or remove a card
-const campfire = {
+export class CampfireRoom extends Room {
+	constructor() {
+		super('campfire')
+	}
 	rest() {
 		console.log('rest')
-	},
+	}
 	upgrade(card) {
 		console.log('upgrade card', card)
-	},
+	}
 	remove(card) {
 		console.log('remove card', card)
 	}
 }
 
-const encounter1 = {
-	enemies: [monster1]
+export class MonsterRoom extends Room {
+	constructor(...monsters) {
+		super('monster')
+		this.monsters = monsters
+	}
+	get isComplete() {
+		const dead = this.monsters.filter(m => m.currentHealth < 1)
+		return dead.length === this.monsters.length
+	}
 }
 
-const encounter2 = {
-	enemies: [monster2]
-}
-
-const encounter3 = {
-	enemies: [monster1, monster1, monster1]
-}
-
-
-// Weak, Strong and Elite monsters
-class Monster {
-	constructor({id, hp, damage}) {
-		this.id = id
-		this.hp = hp
-		this.damage = random(damage)
+// A monster has health, probably some damage and a list of intents.
+// Intents are cycled through as the monster plays its turn.
+export class Monster {
+	constructor(props = {}) {
+		this.id = uuid()
+		this.maxHealth = props.hp || 42
+		this.currentHealth = props.hp || 42
+		this.damage = props.damage || 5
+		// this.damage = random(5)
+		this.intents = ['attack', 'block', 'attack', 'attack']
 	}
 	beforeBattle() {}
-	takeTurn() {}
+	takeTurn() {
+		console.log('takeTurn')
+	}
 }
 
-const monster1 = {
-	intents: []
-}
-
-const monster2 = {}
-
+// export const encounter1 = new MonsterRoom(new Monster())
+// export const encounter2 = new MonsterRoom(new Monster({hp: 24}), new Monster({hp: 20}))
+// export const encounter3 = new MonsterRoom(new Monster(), new Monster(), new Monster())
--- tests/dungeon.js
+++ tests/dungeon.js
@@ -1,14 +1,61 @@
 import test from 'ava'
 import actions from '../public/game/actions'
+import Dungeon, {CampfireRoom, MonsterRoom, Monster} from '../public/game/dungeon'
 
 const a = actions
 
 // Each test gets a fresh game state.
 test.beforeEach(t => {
-	let state = a.createNewGame(state)
+	let state = a.createNewGame()
 	state = a.drawStarterDeck(state)
 	state = a.drawCards(state)
 	t.context = {state}
 })
 
-test.todo('what to do')
+test('can create a dungeon', t => {
+	const simpleDungeon = new Dungeon({
+		rooms: [new MonsterRoom(new Monster()), new CampfireRoom(), new MonsterRoom(new Monster())]
+	})
+
+	t.is(simpleDungeon.rooms.length, 3)
+	t.is(simpleDungeon.rooms[0].type, 'monster')
+	t.is(simpleDungeon.rooms[0].monsters.length, 1)
+	t.is(simpleDungeon.rooms[1].type, 'campfire')
+	t.is(simpleDungeon.rooms[2].type, 'monster')
+})
+
+test('can create rooms with many monsters', t => {
+	const advancedDungeon = new Dungeon({
+		rooms: [new MonsterRoom(new Monster(), new Monster({hp: 100})), new CampfireRoom()]
+	})
+
+	t.is(advancedDungeon.rooms[0].monsters.length, 2)
+	t.is(advancedDungeon.rooms[0].monsters[1].currentHealth, 100)
+})
+
+test('we know when a room is won', t => {
+	const room = new MonsterRoom(new Monster())
+	t.false(room.isComplete)
+	room.monsters[0].currentHealth = 0
+	t.true(room.isComplete)
+})
+
+test('we can navigate a dungeon', t => {
+	const dungeon = new Dungeon({
+		rooms: [new CampfireRoom(), new CampfireRoom(), new CampfireRoom()]
+	})
+	t.deepEqual(dungeon.currentRoom.id, dungeon.rooms[0].id)
+	dungeon.goToNextRoom()
+	t.deepEqual(dungeon.currentRoom.id, dungeon.rooms[1].id)
+	dungeon.goToNextRoom()
+	t.deepEqual(dungeon.currentRoom.id, dungeon.rooms[2].id)
+	t.throws(() => dungeon.goToNextRoom())
+	t.is(dungeon.currentIndex, 2)
+})
+
+test('campfire can rest or upgrade', t => {
+	const camp = new CampfireRoom()
+	t.is(typeof camp.rest, 'function')
+	t.is(typeof camp.upgrade, 'function')
+	t.is(typeof camp.remove, 'function')
+})
