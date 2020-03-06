de36d777ad988880547e6173210790cb5262c306
Oskar Rough <oskar@rough.dk>
2020-03-06 17:37:46 +0100

    Clean up dungeon tests

--- tests/dungeon.js
+++ tests/dungeon.js
@@ -11,10 +11,13 @@ const a = actions
 
 // Each test gets a fresh game state.
 test.beforeEach(t => {
-	let state = a.createNewGame()
-	state = a.drawStarterDeck(state)
-	state = a.drawCards(state)
-	t.context = {state}
+	t.context = {state: a.createNewGame()}
+})
+
+test('can create rooms with many monsters', t => {
+	const room = new MonsterRoom(new Monster(), new Monster({hp: 20}))
+	t.is(room.monsters.length, 2)
+	t.is(room.monsters[1].maxHealth, 20)
 })
 
 test('can create a dungeon', t => {
@@ -28,10 +31,11 @@ test('can create a dungeon', t => {
 	t.is(simpleDungeon.rooms[2].type, 'monster')
 })
 
-test('can create rooms with many monsters', t => {
-	const room = new MonsterRoom(new Monster(), new Monster({hp: 20}))
-	t.is(room.monsters.length, 2)
-	t.is(room.monsters[1].maxHealth, 20)
+test('can set a dungeon', t => {
+	const dungeon = Dungeon({rooms: [MonsterRoom(Monster())]})
+	let state = a.createNewGame()
+	state = a.setDungeon(state, {dungeon})
+	t.deepEqual(state.dungeon, dungeon, 'setting dungeon works')
 })
 
 test('we know when a monster room is won', t => {
@@ -52,12 +56,7 @@ test('we know when a monster room with many monsters is won', t => {
 	t.true(isCurrentRoomCompleted(state))
 })
 
-test.skip('we know when a campfire has been used', t => {
-	const camp = new CampfireRoom()
-	t.false(camp.isComplete)
-	camp.rest()
-	t.true(camp.isComplete)
-})
+test.todo('we know when a campfire has been used')
 
 test('we can navigate a dungeon', t => {
 	// Prepare a game with a dungeon.
@@ -65,32 +64,11 @@ test('we can navigate a dungeon', t => {
 	const dungeon = Dungeon({
 		rooms: [CampfireRoom(), CampfireRoom(), CampfireRoom()]
 	})
-
 	state = a.setDungeon(state, {dungeon})
-	t.deepEqual(state.dungeon, dungeon, 'setting dungeon works')
-
-	// Go to the next room.
+	// Go through the next rooms.
 	state = a.goToNextRoom(state)
-	t.is(state.dungeon.roomNumber, 1)
 	t.is(state.dungeon.rooms[state.dungeon.roomNumber].id, dungeon.rooms[1].id)
-
 	state = a.goToNextRoom(state)
-	t.is(state.dungeon.roomNumber, 2)
 	t.is(state.dungeon.rooms[state.dungeon.roomNumber].id, dungeon.rooms[2].id)
-
-	t.throws(() => a.goToNextRoom(state), null, 'can go further than last room')
-})
-
-test('context...', t => {
-	const {state} = t.context
-	t.falsy(state.dungeon)
-
-	const room = new CampfireRoom()
-	t.is(room.type, 'campfire')
-
-	const dungeon = new Dungeon({rooms: [room]})
-	t.is(dungeon.rooms[0].type, 'campfire')
-	const state2 = actions.setDungeon(state, {dungeon})
-	t.is(state2.dungeon.id, dungeon.id, 'setting a dungeon works?!')
-	t.is(state2.dungeon.rooms[0].type, 'campfire')
+	t.throws(() => a.goToNextRoom(state), null, 'can not go further than last room')
 })
