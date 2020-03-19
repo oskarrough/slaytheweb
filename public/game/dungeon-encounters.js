3e0f2ff78be2247252de76d250a5f371bf28746a
oskarrough <oskar@rough.dk>
2020-03-19 22:29:19 +0100

    Add intents to all monsters in default dungeon

--- public/game/dungeon-encounters.js
+++ public/game/dungeon-encounters.js
@@ -1,15 +1,12 @@
 import Dungeon, {CampfireRoom, MonsterRoom, Monster} from './dungeon.js'
 
+const intents = [{block: 7}, {damage: 10}, {damage: 10}]
+
 // An example dungeon showcasing some different rooms.
 export const createSimpleDungeon = () => {
-	const normal = () =>
-		MonsterRoom(
-			Monster({
-				intents: [{block: 7}, {damage: 10}, {damage: 10}]
-			})
-		)
-	const elite = () => MonsterRoom(Monster({hp: 24}), Monster({hp: 20}))
-	const boss = () => MonsterRoom(Monster({hp: 150}), Monster())
+	const normal = () => MonsterRoom(Monster({intents}))
+	const elite = () => MonsterRoom(Monster({hp: 24, intents}), Monster({hp: 20, intents}))
+	const boss = () => MonsterRoom(Monster({hp: 150, intents}), Monster({intents}))
 	return Dungeon({
 		rooms: [normal(), elite(), CampfireRoom(), boss()]
 	})
