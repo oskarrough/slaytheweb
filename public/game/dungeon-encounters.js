53c46e915e11468ddde5f686263dd92c5bced0a3
Oskar Rough <oskar@rough.dk>
2020-03-20 23:09:30 +0100

    Make the default dungeon AI more interesting

--- public/game/dungeon-encounters.js
+++ public/game/dungeon-encounters.js
@@ -3,11 +3,28 @@ import Dungeon, {CampfireRoom, MonsterRoom, Monster} from './dungeon.js'
 // An example dungeon showcasing some different rooms.
 export const createSimpleDungeon = () => {
 	const intents = [{block: 7}, {damage: 10}, {damage: 10}]
+	// const intents2 = [{damage: 6}, {damage: 5}, {damage: 8}]
+	const dangerousIntents = [
+		{damage: 1},
+		{damage: 2},
+		{damage: 3},
+		{damage: 4},
+		{damage: 5},
+		{damage: 6},
+		{damage: 7},
+		{damage: 8},
+		{damage: 9},
+		{damage: 10},
+		{damage: 15},
+		{damage: 20},
+		{damage: 30},
+		{damage: 50}
+	]
 	const normal = () => MonsterRoom(Monster({intents}))
-	const elite = () => MonsterRoom(Monster({hp: 24, intents}), Monster({hp: 20, intents}))
-	const boss = () => MonsterRoom(Monster({hp: 150, intents}), Monster({intents}))
+	const elite = () =>
+		MonsterRoom(Monster({hp: 24, intents}), Monster({hp: 13, intents: dangerousIntents}))
+	const boss = () => MonsterRoom(Monster({hp: 150, intents}), Monster({intents: dangerousIntents}))
 	return Dungeon({
 		rooms: [normal(), elite(), CampfireRoom(), boss()]
 	})
 }
-
