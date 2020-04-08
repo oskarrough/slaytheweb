814c8fea42c565bb83d1e4b9e53901f4379bde06
Oskar Rough <oskar@rough.dk>
2020-04-08 19:23:54 +0200

    Refactor actionManager

--- public/game/action-manager.js
+++ public/game/action-manager.js
@@ -3,39 +3,35 @@ import actions from '../game/actions.js'
 
 // The action manager makes use of queues to keep track of
 // future and past actions in the game state. Also allowing us to undo.
-// Actions are always run one after the other.
+// Actions are always run one sequentially.
 export default function () {
-	const future = new Queue()
-	const past = new Queue()
-
-	function enqueue(action) {
-		console.log('enqueued', action)
-		future.addToTop({action})
-	}
-
-	// Runs the oldest actions and returns a new state.
-	function dequeue(state) {
-		const {action} = future.takeFromBottom() || {}
-		let nextState
-		if (!action) return
-		try {
-			nextState = actions[action.type](state, action)
-		} catch (err) {
-			throw new Error(err)
-		}
-		past.addToTop({state, action})
-		return nextState
-	}
-
-	function undo() {
-		return this.past.takeFromTop()
-	}
-
 	return {
-		enqueue,
-		dequeue,
-		undo,
-		future,
-		past,
+		future: new Queue(),
+		past: new Queue(),
+
+		// Enqueued items are added to the "future" list.
+		enqueue(action) {
+			future.addToTop({action})
+		},
+
+		// Once dequeued, they end up in the "past".
+		// Returns a new state.
+		dequeue(state) {
+			const {action} = future.takeFromBottom() || {}
+			let nextState
+			if (!action) return
+			try {
+				nextState = actions[action.type](state, action)
+			} catch (err) {
+				throw new Error(err)
+			}
+			past.addToTop({state, action})
+			return nextState
+		},
+
+		// Undo the latest action from the passt.
+		undo() {
+			return this.past.takeFromTop()
+		}
 	}
 }
