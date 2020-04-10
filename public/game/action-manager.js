43d197c73df32c692d1d9cd623f25d76281321dd
Oskar Rough <oskar@rough.dk>
2020-04-10 20:43:01 +0200

    Revert refactor of action manager

--- public/game/action-manager.js
+++ public/game/action-manager.js
@@ -5,33 +5,39 @@ import actions from '../game/actions.js'
 // future and past actions in the game state. Also allowing us to undo.
 // Actions are always run one sequentially.
 export default function () {
-	return {
-		future: new Queue(),
-		past: new Queue(),
+	const future = new Queue()
+	const past = new Queue()
 
-		// Enqueued items are added to the "future" list.
-		enqueue(action) {
-			future.addToTop({action})
-		},
+	// Enqueued items are added to the "future" list.
+	function enqueue(action) {
+		future.addToTop({action})
+	}
 
-		// Once dequeued, they end up in the "past".
-		// Returns a new state.
-		dequeue(state) {
-			const {action} = future.takeFromBottom() || {}
-			let nextState
-			if (!action) return
-			try {
-				nextState = actions[action.type](state, action)
-			} catch (err) {
-				throw new Error(err)
-			}
-			past.addToTop({state, action})
-			return nextState
-		},
-
-		// Undo the latest action from the passt.
-		undo() {
-			return this.past.takeFromTop()
+	// Once dequeued, they end up in the "past".
+	// Returns a new state.
+	function dequeue(state) {
+		const {action} = future.takeFromBottom() || {}
+		let nextState
+		if (!action) return
+		try {
+			nextState = actions[action.type](state, action)
+		} catch (err) {
+			throw new Error(err)
 		}
+		past.addToTop({state, action})
+		return nextState
+	}
+
+	// Undo the latest action from the passt.
+	function undo() {
+		return this.past.takeFromTop()
+	}
+
+	return {
+		enqueue,
+		dequeue,
+		undo,
+		future,
+		past,
 	}
 }
