32b917749164f12ac97e0446c5353bebbb5ef4a0
Jannik Wiggers <Jannik@Janniks-MacBook-Pro.local>
2020-07-01 13:10:33 +0200

    Weiter

--- public/ui/index.js
+++ public/ui/index.js
@@ -50,12 +50,16 @@ class SplashScreen extends Component {
 			ease: 'bounce',
 		})
 		gsap.from('.Splash--fadein details', {delay: 0.1, x: -50, duration: 0.4, opacity: 0})
+
+		gsap.to(".Splash-spoder", {x: 180, y: 40, duration: 3, delay: 5})
 	}
 	render(props) {
 		return html`
 			<article class="Splash Splash--fadein">
 				<h1>Slay the Web</h1>
 				<h2>A little card crawl adventure for you and your browser.</h2>
+				<img class="Splash-spiderweb" src="ui/images/spiderweb.png" />
+				<img class="Splash-spoder" src="ui/images/spoder.png"/>
 				${
 					location.hash &&
 					html`
@@ -66,6 +70,7 @@ class SplashScreen extends Component {
 					`
 				}
 				<p><button autofocus onClick=${props.onNewGame}>Play</a></p>
+
 				<details class="Splash-details">
 					<summary>Tutorial</summary>
 					<p>Slay the Web is a single player card game where you fight monsters to reach the end of the web. It's a game of planning and knowing when to play which card.</p>
