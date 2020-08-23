7fdf0501d1db0ce75d1fc63564db097fab9ff8b5
Samuel <rott.samuel@gmail.com>
2020-08-23 15:17:42 +0200

    oops, went to fast

--- public/ui/app.js
+++ public/ui/app.js
@@ -176,7 +176,7 @@ export default class App extends Component {
 					</div>
 
 					<div class="Hand">
-						<${Cards} isHand=${true} state=${state} />
+						<${Cards} cards=${state.hand} isHand=${true} state=${state} />
 					</div>
 
 					<details class="Menu Overlay" topleft>
@@ -201,11 +201,11 @@ export default class App extends Component {
 					</details>
 					<details class="DrawPile Overlay" bottomleft>
 						<summary>Dr<u>a</u>w pile ${state.drawPile.length}</summary>
-						<${Cards} state=${state} />
+						<${Cards} cards=${state.drawPile} />
 					</details>
 					<details class="DiscardPile Overlay" bottomright>
 						<summary align-right>Di<u>s</u>card pile ${state.discardPile.length}</summary>
-						<${Cards} state=${state} />
+						<${Cards} cards=${state.discardPile} />
 					</details>
 				</div>
 		`
