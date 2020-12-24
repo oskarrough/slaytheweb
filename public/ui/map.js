77f36f86c61ec1e1be3e76635ccd529851057fb8
Oskar Rough <oskar@rough.dk>
2020-12-24 14:39:22 +0100

    Display room type on the map

--- public/ui/map.js
+++ public/ui/map.js
@@ -1,12 +1,17 @@
 import {html} from '../../web_modules/htm/preact/standalone.module.js'
 
 export default function map({dungeon}) {
+	function capitalize(string) {
+		return string.charAt(0).toUpperCase() + string.slice(1)
+	}
 	return html`
 		<h2>Map of the dungeon</h2>
 		<ul class="MapList">
 			${dungeon.rooms.map(
 				(room, index) =>
-					html`<li>Room ${index} ${index === dungeon.index ? '(you are here)' : ''}</li>`
+					html`<li>
+						${index}. ${capitalize(room.type)} ${index === dungeon.index ? '(you are here)' : ''}
+					</li>`
 			)}
 		</ul>
 	`
