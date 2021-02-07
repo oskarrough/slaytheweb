5c349d576ca9844724c9ba914c5b0c09835130bb
Oskar Rough <oskar@rough.dk>
2021-02-07 16:06:28 +0100

    Add debugger to the action manager

--- public/game/index.js
+++ public/game/index.js
@@ -19,7 +19,7 @@ import ActionManager from './action-manager.js'
 // ```
 
 export default function createNewGame() {
-	const actionManager = ActionManager()
+	const actionManager = ActionManager({debug: true})
 
 	// This exists because actions.createNewGame() doesn't set a dungeon and deck.
 	function createNewState() {
