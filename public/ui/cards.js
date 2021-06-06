069cdde15cc99da0582f699bc9f040f032654cfa
Oskar Rough <oskar@rough.dk>
2021-06-06 13:35:49 +0200

    Add more card images

--- public/ui/cards.js
+++ public/ui/cards.js
@@ -22,12 +22,23 @@ export function Card(card, gameState) {
 	const isDisabled = !canPlayCard(card, gameState)
 
 	// 500x380
-	let image = ''
+	let image = 'codices.jpg'
 	if (card.name === 'Strike') image = 'the-angel-of-death.jpg'
 	if (card.name === 'Defend') image = 'angel-messenger.jpg'
 	if (card.name === 'Bash') image = 'apteryx-mantelli.jpg'
 	if (card.name === 'Flourish') image = 'serpentine-dancer.jpg'
 	if (card.name === 'Summer of Sam') image = 'bare-feet-of-god.jpg'
+	if (card.name === '') image = 'codices.jpg'
+	if (card.name === 'Iron Wave') image = 'henry-stares-back.jpg'
+	if (card.name === 'Cleave') image = 'vernal-equinox.jpg'
+	if (card.name === '') image = 'alice-holds-the-white-king.jpg'
+	if (card.name === '') image = '3.jpg'
+	if (card.name === 'Thunderclap') image = 't.jpg'
+	if (card.name === '') image = 'poured-millions-of-bubbles.jpg'
+	if (card.name === '') image = 'railway-trains-in-space.jpg'
+	if (card.name === 'Sucker Punch') image = 'manicule.jpg'
+	if (card.name === 'Clash') image = 'h-sperling-horrified.jpg'
+	image = `/ui/images/cards/${image}`
 
 	return html`
 		<article
@@ -43,7 +54,7 @@ export function Card(card, gameState) {
 					<span>${card.energy}</span>
 				</p>
 				<figure class="Card-media">
-					<img src=${`/ui/images/cards/` + image} alt=${card.name} />
+					<img src=${image} alt=${card.name} />
 				</figure>
 				<h3 class="Card-name">${card.name}</h3>
 				<p class="Card-type">${card.type}</p>
