6bf1bc309acdf1a5f1365c357254edfd8f435756
Oskar Rough <oskar@rough.dk>
2022-08-27 18:14:09 +0200

    Fix typo

--- public/game/cards.js
+++ public/game/cards.js
@@ -3,26 +3,20 @@ import actionMethods from './actions.js'
 import * as conditionMethods from './conditions.js'
 import cards from '../content/cards.js'
 
-export const types = {
-	attack: 'attack',
-	skill: 'skill',
-	power: 'power',
-	status: 'status',
-	curse: 'curse',
-}
-
-// @todo? [NONE, SELF_AND_ENEMY, ALL]
-export const targets = {player: 'player', enemy: 'enemy', 'all enemies': 'all enemies'}
-
 /*
 	This file contains the logic to create cards.
 	While cards are described in an object form, they are always converted to a class equivalent.
 
 	You can find cards themselves in content/cards.js
 
-	Cards can _optionally_ define an array of actions named `actions`. These actions will be run when the card is played.
+	Cards can deal DAMAGE with the `damage` property.
+	Cards can apply BLOCK with the `block` property.
+
+	Cards can apply POWERS with the `powers` object. Powers are hardcoded in the game actions, but feel free to add more.
+
+	Cards can _optionally_ define a list of `actions`. These actions will be run, in defined order, when the card is played.
 	In the same way, you can define a list of `conditions` that have to pass for the card to be playable.
-	Your "actions" actions can ALSO define a list of conditions.
+	You can even add conditions directly on your actions.
 */
 
 // All cards extend this class.
@@ -53,6 +47,18 @@ this.card = {
 color = [RED, GREEN, BLUE, PURPLE, COLORLESS, CURSE]
 rarity = [BASIC, SPECIAL, COMMON, UNCOMMON, RARE, CURSE]
 */
+
+export const types = {
+	attack: 'attack',
+	skill: 'skill',
+	power: 'power',
+	status: 'status',
+	curse: 'curse',
+}
+
+// @todo? [NONE, SELF_AND_ENEMY, ALL]
+export const targets = {player: 'player', enemy: 'enemy', 'all enemies': 'all enemies'}
+
 export class Card {
 	constructor(props) {
 		this.id = uuid()
@@ -70,7 +76,8 @@ export class Card {
 		if (props.upgrade) this.upgrade = props.upgrade
 	}
 	// Runs through a list of actions and return the updated state.
-	// Called when the card is played. Use it for more advanced cards.
+	// Called when the card is played.
+	// You CAN overwrite it, just make sure to return a new state.
 	use(state, {target /*, card*/}) {
 		if (!this.actions) return state
 		let newState = state
