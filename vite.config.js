c11914c5900d00bb123457e9d1ebd738a6abb803
oskarrough <oskar@rough.dk>
2023-07-21 11:15:44 +0200

    Replace custom service worker with vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
 import {resolve} from 'path'
 import {defineConfig} from 'vite'
+import {VitePWA} from 'vite-plugin-pwa'
 
 export default defineConfig({
+	plugins: [
+		VitePWA({
+			registerType: 'autoUpdate',
+			devOptions: {
+				// enabled: true,
+			},
+		}),
+	],
 	build: {
 		rollupOptions: {
 			input: {
-				main: resolve(__dirname, 'index.html'),
-				stats: resolve(__dirname, 'stats.html'),
-				collection: resolve(__dirname, 'collection.html'),
+				index: resolve('index.html'),
+				text: resolve('text.html'),
+				stats: resolve('stats.html'),
+				collection: resolve('collection.html'),
 			},
 		},
 	},
