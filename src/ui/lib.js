0a791bdafd216ab98f80835471ee550aead4816a
oskarrough <oskar@rough.dk>
2023-09-30 18:02:53 +0200

    Also export useEffect, useRef

--- src/ui/lib.js
+++ src/ui/lib.js
@@ -1,5 +1,5 @@
 export {Component, render} from 'preact'
-export {useState} from 'preact/hooks'
+export {useState, useEffect, useRef} from 'preact/hooks'
 export {html} from 'htm/preact'
 
 // This file bundles the things we need in the UI to make the imports easier.
