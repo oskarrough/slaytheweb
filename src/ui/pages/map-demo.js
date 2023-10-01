076871d3ee6098cb90b564a2b20a39d85aea7870
oskarrough <oskar@rough.dk>
2023-10-01 14:28:53 +0200

    Map demo seems to work

--- src/ui/pages/map-demo.js
+++ src/ui/pages/map-demo.js
@@ -1,84 +1,9 @@
-import {html, render, useState, useEffect, useRef} from '../lib.js'
+import {html, render, useState} from '../lib.js'
 import {SlayMap} from '../components/map.js'
-import Dungeon from '../../game/dungeon.js'
-
-const useSyncStyle = (ref, styleKey, inputId, initialValue) => {
-	useEffect(() => {
-		const handleInputChange = (event) => {
-			const value = styleKey.startsWith('--') ? event.target.value : `${event.target.value}px`
-			console.log(styleKey, event.target.value, value)
-			ref.current.base.style.setProperty(styleKey, value)
-		}
-
-		const input = document.getElementById(inputId)
-		input.value = initialValue
-		input.addEventListener('input', handleInputChange)
-
-		return () => {
-			input.removeEventListener('input', handleInputChange)
-		}
-	}, [ref, styleKey, inputId])
-}
-
-const DungeonConfigForm = (props) => {
-	const [config, setConfig] = useState({
-		width: 10,
-		height: 6,
-		minRooms: 2,
-		maxRooms: 5,
-		roomTypes: 'MMMCE',
-		customPaths: '123',
-	})
-
-	const handleInput = (e, field) => {
-		const newConfig = {...config}
-		newConfig[field] = e.target.value
-		setConfig(newConfig)
-	}
-
-	useEffect(() => {
-		const dungeon = Dungeon(config)
-		props.onUpdate(dungeon)
-	}, [config])
-
-	return html`
-		<form>
-			<label>
-				Width
-				<input type="number" value=${config.width} onInput=${(e) => handleInput(e, 'width')} />
-			</label>
-			<label>
-				Height
-				<input type="number" value=${config.height} onInput=${(e) => handleInput(e, 'height')} />
-			</label>
-			<label>
-				Min Rooms
-				<input type="number" value=${config.minRooms} onInput=${(e) => handleInput(e, 'minRooms')} />
-			</label>
-			<label>
-				Max Rooms
-				<input type="number" value=${config.maxRooms} onInput=${(e) => handleInput(e, 'maxRooms')} />
-			</label>
-			<label>
-				Room Types
-				<input type="text" value=${config.roomTypes} onInput=${(e) => handleInput(e, 'roomTypes')} />
-			</label>
-			<label>
-				Custom Paths
-				<input type="text" value=${config.customPaths} onInput=${(e) => handleInput(e, 'customPaths')} />
-			</label>
-		</form>
-	`
-}
+import Dungeon, {defaultOptions as defaultDungeonOptions} from '../../game/dungeon.js'
 
 const Demo = () => {
-	const ref = useRef(null)
 	const [dungeon, setDungeon] = useState(Dungeon())
-
-	// useSyncStyle(ref, '--highlight', 'highlightColor', 'gold')
-	// useSyncStyle(ref, '--pathColor', 'pathColor', 'black')
-	// useSyncStyle(ref, 'width', 'width', '500')
-	// useSyncStyle(ref, 'min-height', 'minHeight', 'none')
 
 	const x = 0
 	const y = 0
@@ -88,31 +13,94 @@ const Demo = () => {
 
 	return html`
 		<div class="Box">
-			<form hidden>
-				<fieldset>
-					<legend>Dungeon settings</legend>
-				</fieldset>
-				<fieldset>
-					<legend>Styles</legend>
-					<label>Highlight color <input type="color" id="highlightColor" value="gold" /></label>
-					<label>Path color <input type="color" id="pathColor" value="black" /></label>
-					<!-- <label>Width <input type="number" id="width" step="20" value="500" /></label> -->
-					<!-- <label>Height <input type="number" id="minHeight" step="100" value="0" /></label> -->
-				</fieldset>
-			</form>
-
-			<${DungeonConfigForm} onUpdate=${(x) => setDungeon(x)} />
+			<${DungeonConfigForm} onUpdate=${(config) => setDungeon(Dungeon(config))} />
 		</div>
 
-		<${SlayMap}
-			ref=${ref}
-			dungeon=${dungeon}
-			x=${x}
-			y=${y}
-			onSelect=${onSelect}
-			disableScatter=${true}
-			debug=${false}
-		><//>
+		${dungeon &&
+		html` <${SlayMap} dungeon=${dungeon} x=${x} y=${y} onSelect=${onSelect} disableScatter=${true} debug=${true}><//>`}
+	`
+}
+
+const DungeonConfigForm = (props) => {
+	const [config, setConfig] = useState(defaultDungeonOptions)
+	const [styles, setStyles] = useState(defaultDungeonOptions)
+
+	const handleInput = (e, field) => {
+		console.log('requested dungeon update', field)
+		const newConfig = {...config}
+		newConfig[field] = e.target.type === 'number' ? Number(e.target.value) : e.target.value
+		setConfig(newConfig)
+		props.onUpdate(newConfig)
+	}
+
+	const handleStyleInput = (e, field) => {
+		console.log('requested style update', field)
+		const el = document.querySelector('slay-map')
+		const {value} = e.target
+		// el.style.setProperty(`--${field}`, value)
+		if (field === 'min-height') {
+			el.style[field] = value + 'vh'
+		} else {
+			el.style[field] = value
+		}
+	}
+
+	return html`
+		<form class="Form">
+			<fieldset>
+				<legend>Dungeon size</legend>
+				<label>
+					Floors
+					<input type="number" value=${config.height} min="0" onInput=${(e) => handleInput(e, 'height')} />
+				</label>
+				<label>
+					Columns
+					<input type="number" value=${config.width} onInput=${(e) => handleInput(e, 'width')} />
+				</label>
+			</fieldset>
+			<fieldset>
+				<legend>Map size</legend>
+				<label>
+					Height
+					<input type="number" value="70" min="0" step="5" onInput=${(e) => handleStyleInput(e, 'min-height')} />vh
+				</label>
+				<label hidden>
+					Width
+					<input type="number" value=${styles.width} onInput=${(e) => handleStyleInput(e, 'width')} />
+				</label>
+			</fieldset>
+			<fieldset>
+				<legend>Amout of rooms per floor</legend>
+				<label>
+					Min
+					<input type="number" value=${config.minRooms} onInput=${(e) => handleInput(e, 'minRooms')} />
+				</label>
+				<label>
+					Max
+					<input type="number" value=${config.maxRooms} onInput=${(e) => handleInput(e, 'maxRooms')} />
+				</label>
+			</fieldset>
+			<fieldset>
+				<legend>Custom rooms & paths</legend>
+				<label>
+					Room Types
+					<input type="text" value=${config.roomTypes} onInput=${(e) => handleInput(e, 'roomTypes')} />
+				</label>
+				<p>M for monster, E for elite, C for camp. Repeat character to increase chance of appearing. There is additional logic in the code as well, which for example increases chance of elites on higher floors.</p>
+				<label>
+					Paths to draw
+					<input
+						type="text"
+						value=${config.customPaths}
+						onInput=${(e) => handleInput(e, 'customPaths')}
+						placeholder="0235"
+					/>
+				</label>
+				<p>
+					Defaults to draw one path per column. To draw paths on specific columns enter a string of indexes like <code>034</code>. This would attempt to draw three paths at those indexes.
+				</p>
+			</fieldset>
+		</form>
 	`
 }
 
