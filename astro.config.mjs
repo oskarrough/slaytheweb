5d540c4ab0e543ee709fd16d8f3d76e68fe2242e
Oskar Rough <oskar@rough.dk>
2024-01-22 10:50:36 +0100

    Try out Sentry again

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import AstroPWA from '@vite-pwa/astro'
 // process.env.VITE_GIT_HASH = execSync('git rev-parse HEAD').toString().trimEnd()
 // process.env.VITE_GIT_MESSAGE = execSync('git show -s --format=%s').toString().trimEnd()
 
+import sentry from '@sentry/astro'
+
 // https://astro.build/config
 export default defineConfig({
 	srcDir: './src/ui',
@@ -13,11 +15,9 @@ export default defineConfig({
 		preact(),
 		AstroPWA({
 			registerType: 'autoUpdate',
-
 			devOptions: {
 				// enabled: true,
 			},
-
 			// The actual webmanifest
 			manifest: {
 				name: 'Slay the Web',
@@ -41,6 +41,14 @@ export default defineConfig({
 						type: 'image/png',
 					},
 				],
+			},
+		}),
+		sentry({
+			dsn: 'https://8dfaea3ae774cfc8d9a79fdac78b2c5d@o4506580528529408.ingest.sentry.io/4506580555268096',
+			sourceMapsUploadOptions: {
+				project: 'slaytheweb',
+				authToken: process.env.SENTRY_AUTH_TOKEN,
+				telemetry: false
 			},
 		}),
 	],
