621226c93e73818179f8958ce7ef5dd2517a3bdf
oskarrough <oskar@rough.dk>
2025-03-24 00:03:39 +0100

    Add deck chooser component

---
+++ src/content/decks.js
@@ -0,0 +1,39 @@
+/**
+ * @typedef {Object} Deck
+ * @property {string} name
+ * @property {string[]} cards
+ */
+
+/** @type {Deck} */
+export const deck1 = {
+	name: 'Classic',
+	cards: ['Defend', 'Defend', 'Defend', 'Defend', 'Strike', 'Strike', 'Strike', 'Strike', 'Strike', 'Bash'],
+}
+
+/** @type {Deck} */
+export const deck2 = {
+	name: 'One of each',
+	cards: [
+		'Adrenaline',
+		'Bash',
+		'Bludgeon',
+		'Body Slam',
+		'Clash',
+		'Cleave',
+		'Defend',
+		'Flourish',
+		'Intimidate',
+		'Iron Wave',
+		'Mask of the Faceless',
+		'Pommel Strike',
+		'Ritual Rain',
+		'Soul Drain',
+		'Strike',
+		'Succube',
+		'Sucker Punch',
+		'Summer of Sam',
+		'Terror',
+		'Thunderclap',
+		'Voodoo Gift',
+	],
+}
